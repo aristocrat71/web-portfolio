08bf447962facd8949860963947f8564689836f0
aristocrat71 <sheth.mitul.71@gmail.com>
2025-05-30 16:14:20 +0530

    Project section css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import projects from '../data/projects.json';
+import githubImg from '../assets/github.png';
+import reactImg from '../assets/react.svg';
 
 const Projects = () => (
   <section id="projects" className="section">
@@ -9,11 +11,13 @@ const Projects = () => (
         <div key={index} className="project-card">
           <h3>{project.title}</h3>
           <p>{project.description}</p>
-          {project.link && (
+          <img src={reactImg} alt={project.title} height={180} width={200}></img>
+          <span>{project.link && (
             <a href={project.link} target="_blank" rel="noopener noreferrer">
-              View Project
+              <img src={githubImg} alt={project.title} height={30} width={30}/>
             </a>
           )}
+          </span>
         </div>
       ))}
     </div>
